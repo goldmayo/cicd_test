c027c779d79573a8c37f74188d46d4ce808d5f96
makisekurisu <kurikame1017@gmail.com>
2024-08-09 00:22:40 +0900

    find id, find password, login, without Oauth,jwt
    //todo: oauth, jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,10 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 exports.registerStep1 = async (req, res) => {
     try {
-        const user = await User.create(req.body);
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = await User.create({ ...req.body, password: hashedPassword });
         res.status(201).json({ message: '가입1 완료. ID 생성됨.', userId: user.id });
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -21,6 +23,50 @@ exports.registerStep2 = async (req, res) => {
         }
     } catch (error) {
         res.status(400).json({ error: error.message });
+    }
+};
 
+exports.findId = async (req, res) => {
+    try {
+        const { representativeName, representativeContact, centerName } = req.query;
+        const user = await User.findOne({ where: { representativeName, representativeContact, centerName } });
+        if (user) {
+            res.status(200).json({ id: user.id });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.findPassword = async (req, res) => {
+    try {
+        const { representativeName, representativeContact, centerName } = req.query;
+        const user = await User.findOne({ where: { representativeName, representativeContact, centerName } });
+        if (user) {
+            res.status(200).json({ password: user.password });
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { id, password } = req.body;
+        const user = await User.findOne({ where: { id } });
+        if (!user) {
+            console.log('###############' + user.password);
+        }
+        if (bcrypt.compare(password, user.password)) {
+            res.status(200).json({ message: '로그인 성공' });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 router.post('/register/step1', userController.registerStep1);
 router.post('/register/step2', userController.registerStep2);
-// router.get('/find-id', userController.findId);
-// router.get('/find-password', userController.findPassword);
-// router.post('/login', userController.login);
+router.get('/find-id', userController.findId);
+router.get('/find-password', userController.findPassword);
+router.post('/login', userController.login);
 
 module.exports = router;

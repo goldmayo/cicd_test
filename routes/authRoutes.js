793de0ea60762a25d1a06aa2cfff2369c3cc84ae
makisekurisu <kurikame1017@gmail.com>
2024-08-09 20:39:04 +0900

    github action test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,6 @@ const { kakaoLogin, kakaoAuth} = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/kakao', kakaoAuth);
-
 router.post('/login/kakao', kakaoLogin);
 
 module.exports = router;

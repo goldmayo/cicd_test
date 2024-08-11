7e3a52b87f7b7ca8c730dee2d32d6bfb43932641
makisekurisu <kurikame1017@gmail.com>
2024-08-11 10:58:25 +0900

    github action test

--- config/database.js
+++ config/database.js
@@ -1,10 +1,29 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     port: 7366,
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     },
+//     logging: console.log,
+// });
+
+// const sequelize = new Sequelize('mydatabase', 'myuser', 'mypassword', {
+//     host: 'mysql', // Docker Compose의 MySQL 서비스 이름
+//     dialect: 'mysql',
+//     // 추가 설정...
+// });
+
+const sequelize = new Sequelize('SteinsGate', 'makisekurisu', 'elpsycongroo', {
+    host: 'seonlim.c7muuq84guhv.ap-northeast-2.rds.amazonaws.com',
     dialect: 'mysql',
-    port: 7366,
+    port: 3306,
     pool: {
         max: 5,
         min: 0,
@@ -13,12 +32,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     },
     logging: console.log,
 });
-
-// const sequelize = new Sequelize('mydatabase', 'myuser', 'mypassword', {
-//     host: 'mysql', // Docker Compose의 MySQL 서비스 이름
-//     dialect: 'mysql',
-//     // 추가 설정...
-// });
 
 sequelize.authenticate()
     .then(() => {
